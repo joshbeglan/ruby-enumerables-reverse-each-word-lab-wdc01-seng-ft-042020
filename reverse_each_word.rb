af13c0b6298329f03186cec49daa979458e21761
Josh Beglan <josh_beglan@yahoo.com>
2020-04-16 20:43:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(sentence)
   end
   new_string.join(" ")
 end
+
+def reverse_each_word(sentence)
+  word = sentence.split(" ")
+  new_string  = []
+  word.collect do |word|
+    reversed = word.reverse
+    new_string << reversed
+  end
+  new_string.join(" ")
+end
